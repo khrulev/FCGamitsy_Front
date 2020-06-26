8bf80e11b300c14cb147ad6fb6def1d17af5b0dc
Ivan Khrulev <subz07@bk.ru>
2020-06-26 19:43:18 +0500

    Updates

---
+++ precache-manifest.9072306e08d52c1ef133d3f23db7e17c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170fb886b52bd328884fba3b7ef6ecc4",
+    "url": "/FCGF/index.html"
+  },
+  {
+    "revision": "cf6f850dd1abfb9a99bd",
+    "url": "/FCGF/static/css/2.503cc5ee.chunk.css"
+  },
+  {
+    "revision": "6ab4df51897aab0c074e",
+    "url": "/FCGF/static/css/main.e95ffced.chunk.css"
+  },
+  {
+    "revision": "cf6f850dd1abfb9a99bd",
+    "url": "/FCGF/static/js/2.bf179d23.chunk.js"
+  },
+  {
+    "revision": "4726d0ca79aece6ab767842b9e4ecc50",
+    "url": "/FCGF/static/js/2.bf179d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab4df51897aab0c074e",
+    "url": "/FCGF/static/js/main.785a471a.chunk.js"
+  },
+  {
+    "revision": "48306e80634695ce1e38",
+    "url": "/FCGF/static/js/runtime-main.8a6fc96b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FCGF/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FCGF/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FCGF/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FCGF/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FCGF/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
